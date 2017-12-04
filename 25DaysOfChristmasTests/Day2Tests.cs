d655a180e73cda5b5273c4c1866ac9fb62ed41be
RomanBlyshchyk <ryblyshc@uwaterloo.ca>
2017-12-04 14:01:53 -0500

    Finished day 2

--- 25DaysOfChristmasTests/Day2Tests.cs
+++ 25DaysOfChristmasTests/Day2Tests.cs
@@ -10,11 +10,21 @@ namespace _25DaysOfChristmasTests
     {
 
         [TestCase("", 0)]
-        [TestCase("1 2 3 \n 4 5 6 \n 7\t8 9", 6)]
+        [TestCase("1 2 3 \r\n 4 5 6 \r\n 7\t8 9", 6)]
+        [TestCase("5 1 9 5\r\n7 5 3\r\n2 4 6 8",18)]
         [Test]
         public void InverseCapchaTest(string input, int result)
         {
             var x = Day2.CorruptionChecksum(input);
+            Assert.AreEqual(result, x);
+        }
+
+        [TestCase("", 0)]
+        [TestCase("5 9 2 8 \r\n 9 4 7 3\r\n3 8 6 5",9)]
+        [Test]
+        public void EvenDivideValuesTest(string input, int result)
+        {
+            var x = Day2.EvenDivideValues(input);
             Assert.AreEqual(result, x);
         }
     }
--- Day2_CorruptionChecksum/Day2.cs
+++ Day2_CorruptionChecksum/Day2.cs
@@ -11,21 +11,69 @@ namespace Day2_CorruptionChecksum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Corruption Checksum:");
-            Console.WriteLine(CorruptionChecksum("1 2 3 \n 4 5 6 \n 7\t8 9"));
+            int part1Result;
+            int part2Result;
+            using (var sr = new StreamReader("C:\\Development\\VS2015\\Projects\\25DaysOfChristmas\\Day2_CorruptionChecksum\\input.txt"))
+            {
+                var input = sr.ReadToEnd();
+                part1Result = CorruptionChecksum(input);
+                part2Result = EvenDivideValues(input);
+            }
+
+            Console.WriteLine($"Corruption Checksum result: {part1Result}");
+            Console.WriteLine($"EvenDivideValues result: {part2Result}");
             Console.ReadKey();
+        }
+
+        public static int EvenDivideValues(string input)
+        {
+            var resultList = new List<int>();
+
+            var lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var values = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var intValues = new List<int>();
+                foreach (var value in values)
+                {
+                    intValues.Add(Convert.ToInt32(value.ToString()));
+                }
+                // Find the only two numbers in each row where one evenly divides the other.
+                // That is, where the result of the division operation is a whole number. 
+                // They would like you to find those numbers on each line, divide them, and add up each line's result.
+                for(int i = 0; i < intValues.Count-1; i++)
+                {
+                    for(int j = i+1; j < intValues.Count; j++)
+                    {
+                        if (intValues[i] % intValues[j] == 0)
+                            resultList.Add(intValues[i] / intValues[j]);
+                        else if (intValues[j] % intValues[i] == 0)
+                            resultList.Add(intValues[j] / intValues[i]);
+                    }
+                }
+
+            }
+
+            var result = 0;
+            resultList.ForEach(x => result += x);
+            return result;
         }
 
         public static int CorruptionChecksum(string input)
         {
             var resultList = new List<int>();
 
-            var lines = input.Split(new char[]{ '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            var lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             foreach (var line in lines)
             {
-                var values = line.Split(new char[] {' ','\t' }, StringSplitOptions.RemoveEmptyEntries);
-                var min = Convert.ToInt32(values.Min().ToString());
-                var max = Convert.ToInt32(values.Max().ToString());
+                var values = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var intValues = new List<int>();
+                foreach (var value in values)
+                {
+                    intValues.Add(Convert.ToInt32(value.ToString()));
+                }
+                var min = intValues.Min();
+                var max = intValues.Max();
                 resultList.Add(max - min);
             }
 
