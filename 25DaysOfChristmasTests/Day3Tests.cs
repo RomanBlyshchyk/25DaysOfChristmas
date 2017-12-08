ab3b9c7a9ce7bd5a2da94ff4f2ad1ae4da0bff5f
Roman Blyshchyk <rblyshc@uwaterloo.ca>
2017-12-07 21:15:07 -0500

    Set up of Day 3

---
+++ 25DaysOfChristmasTests/Day3Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Day3_SpiralMemory;
+
+namespace _25DaysOfChristmasTests
+{
+    [TestFixture]
+    public class Day3Tests
+    {
+
+        [TestCase(0)]
+        [Test]
+        public void TestMethod1(int result)
+        {
+            var x = Day3.SomeMethod();
+            Assert.AreEqual(x, result);
+        }
+    }
+}
---
+++ Day3_SpiralMemory/Day3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_SpiralMemory
+{
+    public class Day3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SomeMethod());
+        }
+
+        public static int SomeMethod()
+        {
+            return 0;
+        }
+    }
+}
