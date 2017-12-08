b3e7e826f2724c8a09c0b1e524859392396ce15b
RomanBlyshchyk <ryblyshc@uwaterloo.ca>
2017-12-08 16:32:13 -0500

    Set up for Day 8

---
+++ Day8_Registers/Day8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_Registers
+{
+    public class Day8
+    {
+        //Example of input:
+        //    b inc 5 if a > 1
+        //    a inc 1 if b< 5
+        //    c dec -10 if a >= 1
+        //    c inc -20 if c == 10
+        static void Main(string[] args)
+        {
+            // create a list of Registers and their values. 
+
+            // split the input into a list of commands
+            // split each command into chunks 
+            //      {register} {operation} {value} {condition}
+            //      {condition} ==> {register} {operator} {value}
+
+
+            // process each line of input one by one
+        }
+    }
+}
