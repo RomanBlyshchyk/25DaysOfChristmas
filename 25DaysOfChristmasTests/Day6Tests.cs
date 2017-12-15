a83a5ee5693ba38774cd9404fb1fa4a4f7799791
RomanBlyshchyk <ryblyshc@uwaterloo.ca>
2017-12-15 17:45:58 -0500

    day 6 set up

--- 25DaysOfChristmasTests/Day6Tests.cs
+++ 25DaysOfChristmasTests/Day6Tests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Day8_Registers;
+using Day6_Memory;
 
 namespace _25DaysOfChristmasTests
 {
---
+++ Day6_Memory/Day6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Memory
+{
+    public class Day6
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
