07183e708e8bf95e85fc697a053f5ed3e3614642
Roman Blyshchyk <rblyshc@uwaterloo.ca>
2017-12-15 22:26:59 -0500

    day 6 complete

--- 25DaysOfChristmasTests/Day6Tests.cs
+++ 25DaysOfChristmasTests/Day6Tests.cs
@@ -11,11 +11,25 @@ namespace _25DaysOfChristmasTests
     [TestFixture]
     class Day6Tests
     {
-
+        [TestCase(new int[] { 0, 2, 7, 0 }, 5)]
+        [TestCase(new int[] { 11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11 }, 5)]
         [Test]
-        public void Day6TestMethod()
+        public void CountCyclesTest(int[] input, int result)
         {
+            var count = Day6.CountCycles(input);
+            Assert.AreEqual(result, count);
+        }
 
+        [TestCase(new int[] { 0, 2, 7, 0 }, new int[] { 2, 4, 1, 2 })]
+        [TestCase(new int[] { 2, 4, 1, 2 }, new int[] { 3, 1, 2, 3 })]
+        [TestCase(new int[] { 3, 1, 2, 3 }, new int[] { 0, 2, 3, 4 })]
+        [TestCase(new int[] { 0, 2, 3, 4 }, new int[] { 1, 3, 4, 1 })]
+        [TestCase(new int[] { 1, 3, 4, 1 }, new int[] { 2, 4, 1, 2 })]
+        [Test]
+        public void PerformRedistributionTest(int[] input, int[] result)
+        {
+            var output = Day6.PerformRedistribution(input);
+            Assert.AreEqual(output, result);
         }
     }
 }
--- Day6_Memory/Day6.cs
+++ Day6_Memory/Day6.cs
@@ -8,8 +8,48 @@ namespace Day6_Memory
 {
     public class Day6
     {
+        // input:
+        // 11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11
         static void Main(string[] args)
         {
+            var input = new int[] { 11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11 };
+            var testinput = new int[] { 0, 2, 7, 0 };
+            CountCycles(input);
+        }
+
+        public static int CountCycles(int[] input)
+        {
+            var history = new List<int[]>();
+            var inputCopy = new int[input.Length];
+            input.CopyTo(inputCopy, 0);
+            history.Add(inputCopy);
+            var newState = PerformRedistribution(input);
+            while (history.FindIndex(x => x.SequenceEqual(newState)) < 0)
+            {
+                var x = new int[input.Length];
+                newState.CopyTo(x,0);
+                history.Add(x);
+                newState = PerformRedistribution(newState);
+            }
+            var cycleCount = history.Count -  history.FindIndex(x => x.SequenceEqual(newState));
+            return history.Count;
+        }
+
+        public static int[] PerformRedistribution(int[] input)
+        {
+            // find largest bloc 
+            var maxValue = input.Max();
+            var indexOfMax = Array.IndexOf(input, input.Max());
+
+            //take the value out of Max and redistribute
+            input[indexOfMax] = 0;
+            while (maxValue != 0)
+            {
+                input[++indexOfMax % input.Length]++;
+                maxValue--;
+            }
+
+            return input;
         }
     }
 }
