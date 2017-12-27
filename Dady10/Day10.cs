efc8dae264521a6695d16c238dee37f4fa3fa8af
RomanBlyshchyk <ryblyshc@uwaterloo.ca>
2017-12-26 22:59:07 -0500

    Day 10 set up and start

--- Dady10/Day10.cs
+++ Dady10/Day10.cs
@@ -8,8 +8,33 @@ namespace Dady10
 {
     public class Day10
     {
+        //input : 187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216
         static void Main(string[] args)
         {
+            var input = new List<int>()
+            {
+                187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216
+            };
+
+
+        }
+
+        public static int KnotHash(List<int> input)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < 256; i++)
+                list.Add(i);
+
+            var skipSize = 0;
+            var startPosition = 0;
+            foreach (var item in input)
+            {
+
+
+                startPosition = (startPosition + item + skipSize) % list.Count;
+                skipSize++;
+            }
+            return 0;
         }
     }
 }
