803bbfaeafb429b97140367db6b427d39e2042a4
RomanBlyshchyk <ryblyshc@uwaterloo.ca>
2017-12-27 15:14:40 -0500

    Day 10 part 1 done; day 11/12 set up

---
+++ 25DaysOfChristmasTests/Day10Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Day10;
+
+namespace _25DaysOfChristmasTests
+{
+    [TestFixture]
+    class Day10Tests
+    {
+
+        [TestCase("187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216", 1980)]
+        [Test]
+        public void KnotHashGameTest(string input, int result)
+        {
+            var list = new List<int>();
+            foreach (var item in input.Split(','))
+            {
+                list.Add(Convert.ToInt32(item.ToString()));
+            }
+
+            var totalScore = Day10.Day10.KnotHashGame(list);
+            Assert.AreEqual(result, totalScore);
+        }
+
+
+        [Test]
+        public void KnotHashTest(string input, string result)
+        {
+            var inputList = new List<int>();
+            foreach (var item in input.ToCharArray())
+            {
+                inputList.Add(Convert.ToInt32(item));
+            }
+            inputList.AddRange(new List<int>() { 17, 31, 73, 47, 23 });
+
+            var totalScore = Day10.Day10.KnotHash(inputList);
+            Assert.AreEqual(result, totalScore);
+        }
+    }
+}
--- Dady10/Day10.cs
+++ Dady10/Day10.cs
@@ -4,37 +4,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dady10
+namespace Day10
 {
     public class Day10
     {
+        private static int _hashSize = 256;
+        private static int _skipSize = 0;
+        private static int _startPosition = 0;
         //input : 187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216
         static void Main(string[] args)
         {
-            var input = new List<int>()
-            {
-                187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216
-            };
+            #region Part 1
+            //var input = new List<int>()
+            //{
+            //    187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216
+            //};
 
+            //var input2 = new List<int>()
+            //{
+            //    3,4,1,5
+            //};
+
+            //Console.WriteLine(KnotHashRound(input)); 
+            #endregion
+
+            var stringInput = "1,2,3";
+            var input = new List<int>();
+            foreach (var item in stringInput.ToCharArray())
+            {
+                input.Add(Convert.ToInt32(item));
+            }
+            input.AddRange(new List<int>() { 17, 31, 73, 47, 23 });
+            Console.WriteLine(KnotHash(input));
 
         }
 
-        public static int KnotHash(List<int> input)
+        public static string KnotHash(List<int> input)
         {
             var list = new List<int>();
-            for (int i = 0; i < 256; i++)
+            for (int i = 0; i < _hashSize; i++)
                 list.Add(i);
 
-            var skipSize = 0;
-            var startPosition = 0;
+            for (int i = 0; i < 64; i++)
+                KnotHashRound(input, list);
+
+            // calculate dense hash --> 16 numbers 
+            var denseHash = new List<int>();
+            for (int i = 0; i < _hashSize; i += 16)
+            {
+                var subset = list.GetRange(i, 16);
+                var denseValue = 0;
+                foreach (var item in subset)
+                {
+                    denseValue = denseValue | item;
+                    denseHash.Add(denseValue);
+                }
+            }
+
+            // convert each number into hex value and combine into 32 hex digit value
+            var hexValue = "";
+            foreach (var item in denseHash)
+            {
+                // convert item to hex....somehow
+                hexValue += "";
+            }
+
+            return hexValue;
+        }
+
+        private static void KnotHashRound(List<int> input, List<int> list)
+        {
             foreach (var item in input)
             {
+                if (_startPosition + item <= list.Count)
+                {
+                    list.Reverse(_startPosition, item);
+                }
+                else
+                {
+                    var startCount = (_startPosition + item) % (list.Count);
+                    var endCount = item - startCount;
 
+                    var selectedList = list.GetRange(_startPosition, endCount);
+                    selectedList.AddRange(list.GetRange(0, startCount));
+                    selectedList.Reverse();
 
-                startPosition = (startPosition + item + skipSize) % list.Count;
-                skipSize++;
+                    // replace items in the original list with the changed selection
+                    list.RemoveRange(_startPosition, endCount);
+                    list.RemoveRange(0, startCount);
+
+                    list.AddRange(selectedList.GetRange(0, endCount));
+                    list.InsertRange(0, selectedList.GetRange(endCount, startCount));
+                }
+
+                _startPosition = (_startPosition + item + _skipSize) % list.Count;
+                _skipSize++;
             }
-            return 0;
+        }
+
+        public static int KnotHashGame(List<int> input)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < _hashSize; i++)
+                list.Add(i);
+
+            KnotHashRound(input, list);
+
+            var result = list[0] * list[1];
+            return result;
         }
     }
 }
