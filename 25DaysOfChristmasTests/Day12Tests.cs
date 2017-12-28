4893db23792a7ef638940a479325fb36895a5a67
romanblyshchyk <ryblyshc@uwaterloo.ca>
2017-12-28 15:10:15 -0500

    Day 12 set up

---
+++ 25DaysOfChristmasTests/Day12Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace _25DaysOfChristmasTests
+{
+    [TestFixture]
+    class Day12Tests
+    {
+
+        [TestCase("", 0)]
+        [Test]
+        public void FindGroupSizeTest(string input, int result)
+        {
+            var totalScore = Day12.Day12.FindGroupSize(input);
+            Assert.AreEqual(result, totalScore);
+        }
+    }
+}
--- Day12/Day12.cs
+++ Day12/Day12.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace Day12
     {
         static void Main(string[] args)
         {
+
+            using (var sr = new StreamReader("C:\\Development\\VS2015\\Projects\\25DaysOfChristmas\\Day12\\input.txt"))
+            {
+                Console.WriteLine(FindGroupSize(sr.ReadToEnd()));
+            }
+        }
+
+        public static int FindGroupSize(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var groupSize = 0;
+
+            foreach (var line in lines)
+            {
+                var lineParts = line.Split(' ');
+                var program = lineParts[0];
+                var friends = new List<string>();
+
+                for (int i = 2; i < lineParts.Length; i++)
+                {
+                    friends.Add(lineParts[i]);
+                }
+            }
+
+            return 0;
         }
     }
 }
