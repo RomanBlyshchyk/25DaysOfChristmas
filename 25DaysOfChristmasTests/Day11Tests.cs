2f38b0519e8609e72847129c5b63bc29059070ae
Roman Blyshchyk <rblyshc@uwaterloo.ca>
2017-12-27 19:35:40 -0500

    day 11 part 1 - partial

---
+++ 25DaysOfChristmasTests/Day11Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace _25DaysOfChristmasTests
+{
+    [TestFixture]
+    class Day11Tests
+    {
+
+        [TestCase("", 0)]
+        [TestCase("n", 1)]
+        [TestCase("n,n,n",3)]
+        [TestCase("s,s,s,s,s,s,s,s",8)]
+        [TestCase("n,s,n,s",0)]
+        [TestCase("ne,ne,ne,ne",4)]
+        [TestCase("ne,ne,s", 2)]
+        [TestCase("nw,nw,nw", 3)]
+        [TestCase("se,se,se", 3)]
+        [TestCase("sw,sw,sw,sw", 4)]
+        [TestCase("ne,ne,sw,sw", 0)]
+        [TestCase("ne,ne,s,s", 2)]
+        [TestCase("se,sw,se,sw,sw", 3)]
+        [TestCase("n,se,s,sw,nw,n,ne", 1)]
+        [TestCase("n,se,s,sw,nw,n,ne,s", 0)]
+        [TestCase("n,n,se,se,s,s,sw,sw,nw,nw,n,n,ne,ne", 2)]
+        [TestCase("n,n,se,se,s,s,sw,sw,nw,nw,n,n,ne,ne,s,s", 0)]
+        [TestCase("s,ne,n,nw,sw,s,se,n",0)]
+        [Test]
+        public void FindShortestDistanceTest(string input, int result)
+        {
+            var totalScore = Day11.Day11.FindShortestDistance(input);
+            Assert.AreEqual(result, totalScore);
+        }
+    }
+}
--- Day11/Day11.cs
+++ Day11/Day11.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,151 @@ namespace Day11
     {
         static void Main(string[] args)
         {
+            using (var sr = new StreamReader(@"C:\Users\Roman\Source\Repos\25DaysOfChristmas\Day11\input.txt"))
+            {
+                Console.WriteLine(FindShortestDistance(sr.ReadToEnd()));
+                Console.ReadKey();
+            }
+        }
+
+        public static int FindShortestDistance(string input)
+        {
+            var stepsNorth = 0;
+            var stepsSouth = 0;
+            var stepsEast = 0;
+            var stepsWest = 0;
+
+            var steps = input.Split(',');
+
+            foreach (var step in steps)
+            {
+                switch (step)
+                {
+                    case "n":
+                        if (stepsSouth != 0 && (stepsEast != 0 || stepsWest != 0))
+                        {
+                            stepsSouth--;
+                        }
+                        else if (stepsSouth != 0)
+                        {
+                            stepsSouth--;
+                        }
+                        else
+                        {
+                            stepsNorth++;
+                        }
+                        break;
+                    case "ne":
+
+                        if (stepsWest != 0 && stepsSouth != 0)
+                        {
+                            stepsWest--;
+                            stepsSouth--;
+                        }
+                        else if (stepsWest != 0)
+                        {
+                            stepsWest--;
+                            stepsNorth++;
+                        }
+                        else if (stepsSouth != 0)
+                        {
+                            stepsSouth--;
+                            stepsEast++;
+                        }
+                        else
+                        {
+                            stepsNorth++;
+                            stepsEast++;
+                        }
+
+                        break;
+                    case "nw":
+                        if (stepsEast != 0 && stepsSouth != 0)
+                        {
+                            stepsEast--;
+                            stepsSouth--;
+                        }
+                        else if (stepsEast != 0)
+                        {
+                            stepsEast--;
+                            stepsNorth++;
+                        }
+                        else if (stepsSouth != 0)
+                        {
+                            stepsSouth--;
+                            stepsWest++;
+                        }
+                        else
+                        {
+                            stepsNorth++;
+                            stepsWest++;
+                        }
+                        break;
+                    case "s":
+                        if (stepsNorth != 0 && (stepsEast != 0 || stepsWest != 0))
+                        {
+                            stepsNorth--;
+                        }
+                        else if (stepsNorth != 0)
+                        {
+                            stepsNorth--;
+                        }
+                        else
+                        {
+                            stepsSouth++;
+                        }
+                        break;
+                    case "se":
+                        if (stepsNorth != 0 && stepsWest != 0)
+                        {
+                            stepsNorth--;
+                            stepsWest--;
+                        }
+                        else if (stepsNorth != 0)
+                        {
+                            stepsNorth--;
+                            stepsEast++;
+                        }
+                        else if (stepsWest != 0)
+                        {
+                            stepsWest--;
+                            stepsSouth++;
+                        }
+                        else
+                        {
+                            stepsSouth++;
+                            stepsEast++;
+                        }
+                        break;
+                    case "sw":
+                        if (stepsNorth != 0 && stepsEast != 0)
+                        {
+                            stepsNorth--;
+                            stepsEast--;
+                        }
+                        else if (stepsNorth != 0)
+                        {
+                            stepsNorth--;
+                            stepsWest++;
+                        }
+                        else if (stepsEast != 0)
+                        {
+                            stepsEast--;
+                            stepsSouth++;
+                        }
+                        else
+                        {
+                            stepsSouth++;
+                            stepsWest++;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Something went wrong in switch statement");
+                        break;
+                }
+            }
+            
+            return new List<int>() { stepsNorth, stepsSouth, stepsEast, stepsWest }.Max();
         }
     }
 }
