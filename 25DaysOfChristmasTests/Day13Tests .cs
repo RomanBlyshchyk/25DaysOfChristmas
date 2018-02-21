708c01e6133d7b1c3cfb9ef3aa2bdec995a6e3a6
romanblyshchyk <ryblyshc@uwaterloo.ca>
2018-02-21 11:05:04 -0500

    Day 13 set up

---
+++ 25DaysOfChristmasTests/Day13Tests .cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Day13;
+
+namespace _25DaysOfChristmasTests
+{
+    [TestFixture]
+    class Day13Tests
+    {
+        private const string input = @"0: 3
+                                        1: 2
+                                        4: 4
+                                        6: 4";
+
+
+        [TestCase(input, 24)]
+        [Test]
+        public void FindGroupSizeTest(string input, int result)
+        {
+            var totalScore = Day13.Day13.FindSeverity(input);  //
+            Assert.AreEqual(result, totalScore);
+        }
+
+    }
+}
---
+++ Day13/Day13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    public class Day13
+    {
+        static void Main(string[] args)
+        {
+            //"C:\\Development\\VS2015\\Projects\\25DaysOfChristmas\\Day13\\input.txt"
+            //@"c:\users\roman\source\repos\25daysofchristmas\day13\input.txt"
+            using (var sr = new StreamReader("C:\\Development\\VS2015\\Projects\\25DaysOfChristmas\\Day13\\input.txt"))
+            {
+                Console.WriteLine(FindSeverity(sr.ReadToEnd()));
+                Console.ReadKey();
+            }
+        }
+
+        public static bool FindSeverity(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
