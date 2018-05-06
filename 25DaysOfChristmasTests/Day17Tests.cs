8f9b09c619a839298c437ce3e8cec48c0abb4968
romanblyshchyk <ryblyshc@uwaterloo.ca>
2018-05-05 21:56:02 -0400

    Day 17 part 1 - DONE

---
+++ 25DaysOfChristmasTests/Day17Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Day16;
+
+namespace _25DaysOfChristmasTests
+{
+    [TestFixture]
+    class Day17Tests
+    {
+        // abcde
+
+        [TestCase("s1", "eabcd")]
+        [TestCase("s4", "bcdea")]
+        [TestCase("s0", "abcde")]
+        [TestCase("x2/4", "abedc")]
+        [TestCase("pb/e", "aecdb")]
+        [TestCase("s1,x3/4,pe/b", "baedc")]
+        [TestCase("s3","cdeab")]
+        [TestCase("s3,pc/e","edcab")]
+        [TestCase("s3,pc/e,x0/4","bdcae")]
+        [Test]
+        public void Test(string input, string result)
+        {
+            var state = Day16.Day16.LetsDance(input);
+            Assert.AreEqual(result, state);
+        }
+
+
+
+    }
+}
---
+++ Day17/Day17.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+    class Day17
+    {
+        public static void Main(string[] args)
+        {
+            var list = new CircularLinkedList<int>(0);
+
+            for (int i = 0; i < 2017; i++)
+            {
+                for (int j = 0; j < 349; j++)
+                {
+                    list.Next();
+                }
+
+                list.AddAfter(list.currentPosition, i + 1);
+                list.Next();
+            }
+
+            Console.WriteLine(list.currentPosition.Next.Value);
+            Console.ReadKey();
+        }
+    }
+
+    public class CircularLinkedList<T> : LinkedList<T>
+    {
+        public LinkedListNode<T> currentPosition;
+
+
+        public CircularLinkedList(T firstValue)
+        {
+            AddFirst(new LinkedListNode<T>(firstValue));
+            currentPosition = First;
+        }
+
+
+        public void Next()
+        {
+            currentPosition = currentPosition.Next ?? currentPosition.List.First;
+        }
+    }
+}
